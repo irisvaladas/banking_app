62d6c7fa84d7a693ae90f22ad2dea6837bf97342
irisvaladas <irismprlval@hotmail.com>
2022-07-28 16:12:25 +0100

    db_utils - db_get_customer_info, app.py- route for customer details, html - display.html

--- bank_app.py
+++ bank_app.py
@@ -1,7 +1,17 @@
-# from flask import Flask, jsonify, request
-# from bank_db_utils import (name of function returning json from mysql)
-#
-# app = Flask(__name__)
+from flask import Flask, request, render_template,redirect, url_for, session
+from bank_db_utils import Customer
+
+app = Flask(__name__)
+
+@app.route('/customer_details/<custid>')
+def get_customer_info(custid):
+    cust = Customer(custid)
+    res = cust.db_get_customer_info()[0]
+    return render_template("display.html", account=res)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
+
 #
 # Example
 # @app.route('/students/<student_id>')
@@ -9,5 +19,3 @@
 #     res = db_get_student_info(student_id)
 #     return jsonify(res)
 #
-# if __name__ == '__main__':
-#     app.run(debug=True, port=5001)
--- bank_db_utils.py
+++ bank_db_utils.py
@@ -1,44 +1,80 @@
-# import mysql.connector
-# from config import USER, PASSWORD, HOST
-#
-# def _connect_to_db(db_name):
-#     cnx = mysql.connector.connect(
-#         host=HOST,
-#         user=USER,
-#         password=PASSWORD,
-#         database=db_name
-#     )
-#     return cnx
-#
-# class DbConnectionError(Exception):
-#     pass
-#
-# def db_get_student_info(student_id):
-#     result = None
-#     db_connection = None
-#     try:
-#         db_name = 'COURSE'
-#         db_connection = _connect_to_db(db_name)
-#         cur = db_connection.cursor(dictionary=True)
-#         print("Connected to DB: %s" % db_name)
-#         query = """
-#             SELECT
-#                 s.firstName, s.lastName, c.subject, t.firstName AS teacherFirstName, t.lastName AS teacherLastName
-#             FROM Student s
-#             JOIN Course c
-#             ON c.id = s.courseId
-#             JOIN Teacher t
-#             ON t.id = c.teacherId
-#             WHERE s.id = %s;
-#             """
-#         data = (student_id,)
-#         cur.execute(query, data)
-#         result = cur.fetchall()  # this is a list with db records where each record is a tuple
-#         cur.close()
-#     except Exception:
-#         raise DbConnectionError("Failed to read data from DB")
-#     finally:
-#         if db_connection:
-#             db_connection.close()
-#             print("DB connection is closed")
-#         return result
+import mysql.connector
+from config import USER, PASSWORD, HOST
+
+
+class DbConnectionError(Exception):
+    pass
+
+
+def _connect_to_db(db_name):
+    cnx = mysql.connector.connect(
+        host=HOST,
+        user=USER,
+        password=PASSWORD,
+        database=db_name
+    )
+    return cnx
+
+class Customer:
+    def __init__(self, custid):
+        self.custid = custid
+
+    def db_get_customer_info(self):
+        result = None
+        db_connection = None
+        try:
+            db_name = 'bank'
+            db_connection = _connect_to_db(db_name)
+            cur = db_connection.cursor(dictionary=True)
+            print("Connected to DB: %s" % db_name)
+            query = """
+                SELECT
+                    c.fname, c.mname, c.ltname, c.city, c.mobileno, c.occupation, c.dob
+                FROM customer c
+                where
+                c.custid = %s
+                """
+            data = (self.custid,)
+            cur.execute(query, data)
+            result = cur.fetchall()  # this is a list with db records where each record is a tuple
+            cur.close()
+        except Exception:
+            raise DbConnectionError("Failed to read data from DB")
+        finally:
+            if db_connection:
+                db_connection.close()
+                print("DB connection is closed")
+            return result
+
+    def db_update_costumer_info(self, fname, mname, ltname, city, mobileno, occupation, dob):
+        self.fname = fname
+        self.mname = mname
+        self.ltname = ltname
+        self.city = city
+        self.mobileno = mobileno
+        self.occupation = occupation
+        self.dob = dob
+        db_connection = None
+        try:
+            db_name = 'bank'
+            db_connection = _connect_to_db(db_name)
+            cur = db_connection.cursor()
+            print("Connected to DB: %s" % db_name)
+            query = """
+                UPDATE customer
+                Set fname = %s, mname = %s, ltname = %s, city = %s, mobileno = %s, occupation = %s, dob = %s
+                where custid = "C00001";
+                """
+            data = (fname, mname, ltname, city, mobileno, occupation, dob)
+            cur.executemany(query, data)  # this is a list with db records where each record is a tuple
+            db_connection.commit()
+            cur.close()
+        except Exception:
+            raise DbConnectionError("Failed to read data from DB")
+        finally:
+            if db_connection:
+                db_connection.close()
+                print("DB connection is closed")
+
+ramesh = Customer("C00001")
+print(ramesh.db_get_customer_info())
